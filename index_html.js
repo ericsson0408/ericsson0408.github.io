ce99926d8374d49b33d6450249eff6deeefb11da
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-08-13 18:28:49 +0800

    Update index_html.js

--- index_html.js
+++ index_html.js
@@ -1,7 +1,6 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
-document.getElementById("cal").innerHTML = 0;
 function Function(){
   document.getElementById("cal").innerHTML = parseInt(document.getElementById("cal").innerHTML) + 1;
 
