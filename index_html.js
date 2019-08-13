bdc5f5a9c9596a2b68d9540516f3b822d7126561
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-08-13 18:19:42 +0800

    Update index_html.js

--- index_html.js
+++ index_html.js
@@ -2,8 +2,7 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 var a = 10;
-$(".a").text(a);
 function Function(){
 a+=1;
-$(".a").text(a);
+document.write(a);
 }
