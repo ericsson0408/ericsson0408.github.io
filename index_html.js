953ac87c1100e9164ac517b460af7c75ffbba463
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-08-15 20:18:40 +0800

    Update index_html.js

--- index_html.js
+++ index_html.js
@@ -9,6 +9,6 @@ function add() {
     var b = parseInt(document.getElementById('b').value);
     document.getElementById('result').value =  a+b ; 
 }
-function candy() {
-    document.getElementById('result').value += 1 ; 
+function plus() {
+    document.getElementById('candy').value += 1 ; 
 }
