300a3386d8201a2d230f029104eb4eb74eb5c7e9
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-08-16 00:51:54 +0800

    Update index_html.js

--- idle/index_html.js
+++ idle/index_html.js
@@ -1,3 +1,12 @@
-function plus() {
-    document.getElementById("money").value = parseInt(document.getElementById("money").value) + 1; 
+function gather() {
+    document.getElementById("meat").value = parseInt(document.getElementById("meat").value) + 1; 
 }
+function hunt() {
+    document.getElementById("hunt").value = parseInt(document.getElementById("hunt").value) + 1;
+    document.getElementById("meat").value = parseInt(document.getElementById("meat").value) - 10; 
+}
+var myVar = setInterval(myTimer, 1000);
+function myTimer() {
+    document.getElementById("meat").value = parseInt(document.getElementById("meat").value) + 1;
+}
+
