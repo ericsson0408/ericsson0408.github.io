7ab7b7d867d03b5d65f660e5be819cc8b78fd1c2
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-08-17 01:00:48 +0800

    Create index_html.js

---
+++ tetris/index_html.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function Function(){
+  document.getElementById("cal").innerHTML = parseInt(document.getElementById("cal").innerHTML) + 1;
+}
+function add() {
+    var a = parseInt(document.getElementById('a').value);
+    var b = parseInt(document.getElementById('b').value);
+    document.getElementById('result').value =  a+b ; 
+}
+function plus() {
+    document.getElementById("candy").value = parseInt(document.getElementById("candy").value) + 1; 
+}
