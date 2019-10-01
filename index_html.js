b02814cafcfab6920ffa7f8009e9f4435aa21727
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-01 13:54:04 +0800

    Update index_html.js

--- index_html.js
+++ index_html.js
@@ -22,6 +22,17 @@ function CP1(){
     if( document.getElementById('CP1').style.display=="block"){
     document.getElementById('CP1').style.display="none";
     }
-    else{document.getElementById('CP1').style.display="block";}
-    
+    else{document.getElementById('CP1').style.display="block";}  
+}
+function CP(){
+    if( document.getElementById('CPall').style.display=="block"){
+    document.getElementById('CPall').style.display="none";
+    }
+    else{document.getElementById('CPall').style.display="block";}  
+}
+function DXII(){
+    if( document.getElementById('DXIIall').style.display=="block"){
+    document.getElementById('DXIIall').style.display="none";
+    }
+    else{document.getElementById('DXIIall').style.display="block";}  
 }
