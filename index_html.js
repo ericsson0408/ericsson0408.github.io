1811d038dd171cd5f9b85bf7934f92cbb3d45e90
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-03 00:14:31 +0800

    Update index_html.js

--- index_html.js
+++ index_html.js
@@ -24,6 +24,12 @@ function CP1(){
     }
     else{document.getElementById('CP1').style.display="block";}  
 }
+function CP2(){
+    if( document.getElementById('CP2').style.display=="block"){
+    document.getElementById('CP2').style.display="none";
+    }
+    else{document.getElementById('CP2').style.display="block";}  
+}
 function CP(){
     if( document.getElementById('CPall').style.display=="block"){
     document.getElementById('CPall').style.display="none";
