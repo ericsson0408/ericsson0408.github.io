48dc2c83012094fcecb7368235537d5027214283
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-03 15:44:04 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -4,14 +4,15 @@ var Year=Today.getFullYear();
 var Month=Today.getMonth()+1;
 var Date=Today.getDate();
 function CD1(){
-document.getElementById("CD1").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+var a=document.getElementById("D1").innerHTML
+document.getElementById("CD1").innerHTML=a
 }
 function CD2(){
-document.getElementById("CD1").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+document.getElementById("CD2").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
 }
 function CD3(){
-document.getElementById("CD1").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+document.getElementById("CD3").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
 }
 function CD4(){
-document.getElementById("CD1").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+document.getElementById("CD4").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
 }
