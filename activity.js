c4840aeb5948d3fa0b86b2c376e7b0db3bd24c3b
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-03 15:41:50 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -6,3 +6,12 @@ var Date=Today.getDate();
 function CD1(){
 document.getElementById("CD1").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
 }
+function CD2(){
+document.getElementById("CD1").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+}
+function CD3(){
+document.getElementById("CD1").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+}
+function CD4(){
+document.getElementById("CD1").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+}
