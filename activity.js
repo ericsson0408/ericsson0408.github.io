513d809136edf4cf6cda16388823c9ab643581af
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-03 16:56:08 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -3,7 +3,7 @@ var Today=new Date();
 var YearT=Today.getFullYear();
 var MonthT=Today.getMonth()+1;
 var DateT=Today.getDate();
-function Days( Month , Date){
+function Days(Month, Date){
 var Day=0;
 if (Month=='2'){Day+=31;}
 if (Month=='3'){Day+=59;}
@@ -27,14 +27,14 @@ var Year_1=parseInt(a[0]+a[1]+a[2]);
 var Month_1=parseInt(a[4]+a[5]);
 var Date_1=parseInt(a[7]+a[8]);
 var count=Days(Month_1,Date_1)-DayT;
-document.getElementById("CD1").innerHTML="倒數"+DayT+"天";
+document.getElementById("CD1").innerHTML="倒數"+DayT+"天"+;
 }
 function CD2(){
-document.getElementById("CD2").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+document.getElementById("CD2").innerHTML="今天日期是"+YearT+"年"+MonthT+"月"+DateT+"日";
 }
 function CD3(){
-document.getElementById("CD3").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+document.getElementById("CD3").innerHTML="今天日期是"+YearT+"年"+MonthT+"月"+DateT+"日";
 }
 function CD4(){
-document.getElementById("CD4").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
+document.getElementById("CD4").innerHTML="今天日期是"+YearT+"年"+MonthT+"月"+DateT+"日";
 }
