06964768774f47e0f473011b758adb4cb577185f
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-03 16:02:04 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -3,12 +3,26 @@ var Today=new Date();
 var Year=Today.getFullYear();
 var Month=Today.getMonth()+1;
 var Date=Today.getDate();
+var Days=0;
+if (Month=='2'){Days+=31;}
+if (Month=='3'){Days+=59;}
+if (Month=='4'){Days+=90;}
+if (Month=='5'){Days+=120;}
+if (Month=='6'){Days+=151;}
+if (Month=='7'){Days+=181;}
+if (Month=='8'){Days+=212;}
+if (Month=='9'){Days+=243;}
+if (Month=='10'){Days+=273;}
+if (Month=='11'){Days+=304;}
+if (Month=='12'){Days+=334;}
+Days+=Date;
+
 function CD1(){
 var a=document.getElementById("D1").innerHTML;
 Year_1=parseInt(a[0]+a[1]+a[2]);
 Month_1=parseInt(a[4]+a[5]);
 Date_1=parseInt(a[7]+a[8]);
-document.getElementById("CD2").innerHTML=Year_1+Month_1+Date_1;
+document.getElementById("CD1").innerHTML=Days;
 }
 function CD2(){
 document.getElementById("CD2").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
