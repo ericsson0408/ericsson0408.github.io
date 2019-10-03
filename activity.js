39000d12b43023ac1e1219ee358922f28cecbd8d
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-03 15:44:19 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -4,8 +4,8 @@ var Year=Today.getFullYear();
 var Month=Today.getMonth()+1;
 var Date=Today.getDate();
 function CD1(){
-var a=document.getElementById("D1").innerHTML
-document.getElementById("CD1").innerHTML=a
+var a=document.getElementById("D1").innerHTML;
+document.getElementById("CD1").innerHTML=a;
 }
 function CD2(){
 document.getElementById("CD2").innerHTML="今天日期是"+Year+"年"+Month+"月"+Date+"日";
