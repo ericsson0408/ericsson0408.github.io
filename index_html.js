c0dbd04ec7ea95a79feafccbbe7df44ac03a1cc1
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-03 08:45:05 +0800

    Update index_html.js

--- index_html.js
+++ index_html.js
@@ -18,27 +18,3 @@ function check(){
     }
     else{alert("輸入錯誤")}
 }
-function CP1(){
-    if( document.getElementById('CP1').style.display=="block"){
-    document.getElementById('CP1').style.display="none";
-    }
-    else{document.getElementById('CP1').style.display="block";}  
-}
-function CP2(){
-    if( document.getElementById('CP2').style.display=="block"){
-    document.getElementById('CP2').style.display="none";
-    }
-    else{document.getElementById('CP2').style.display="block";}  
-}
-function CP(){
-    if( document.getElementById('CPall').style.display=="block"){
-    document.getElementById('CPall').style.display="none";
-    }
-    else{document.getElementById('CPall').style.display="block";}  
-}
-function DXII(){
-    if( document.getElementById('DXIIall').style.display=="block"){
-    document.getElementById('DXIIall').style.display="none";
-    }
-    else{document.getElementById('DXIIall').style.display="block";}  
-}
