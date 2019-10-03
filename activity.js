9fb0f6e4be652f04b309f17a04dcbdd150c45aa1
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-03 15:26:48 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -1,3 +1,3 @@
 
 var Today=new Date();
-document.write("今天日期是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
+document.getElementById("CD1").innerHTML="今天日期是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
