ff53ea8c9c9bf9dc80e92ade1005163aa0f0f6e9
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-08 18:39:20 +0800

    Update history.js

--- history.js
+++ history.js
@@ -1,3 +1,9 @@
+function CP(){
+    if( document.getElementById('CPall').style.display=="block"){
+    document.getElementById('CPall').style.display="none";
+    }
+    else{document.getElementById('CPall').style.display="block";}  
+}
 function CP1(){
     if( document.getElementById('CP1').style.display=="block"){
     document.getElementById('CP1').style.display="none";
@@ -10,15 +16,18 @@ function CP2(){
     }
     else{document.getElementById('CP2').style.display="block";}  
 }
-function CP(){
-    if( document.getElementById('CPall').style.display=="block"){
-    document.getElementById('CPall').style.display="none";
-    }
-    else{document.getElementById('CPall').style.display="block";}  
-}
+
+
 function DXII(){
     if( document.getElementById('DXIIall').style.display=="block"){
     document.getElementById('DXIIall').style.display="none";
     }
     else{document.getElementById('DXIIall').style.display="block";}  
+}
+
+function HK1(){
+    if( document.getElementById('HK1').style.display=="block"){
+    document.getElementById('HK1').style.display="none";
+    }
+    else{document.getElementById('HK1').style.display="block";}  
 }
