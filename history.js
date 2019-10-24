15f783c6f314b51ecc571bd1b867160336f64bff
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2019-10-24 10:37:07 +0800

    Update history.js

--- history.js
+++ history.js
@@ -17,6 +17,21 @@ function CP(){  BTall("CP");    }
 function CP1(){ BT("CP1");      }
 function CP2(){ BT("CP2");      }
 
+function OC(){ BTall("OC");     }
+function OC1(){ BT("OC1");      }
+function OC2(){ BT("OC2");      }
+function OC3(){ BT("OC3");      }
+function OC4(){ BT("OC4");      }
+function OC5(){ BT("OC5");      }
+function OC6(){ BT("OC6");      }
+function OC7(){ BT("OC7");      }
+function OC8(){ BT("OC8");      }
+function OC9(){ BT("OC9");      }
+function OC10(){ BT("OC10");      }
+function OC11(){ BT("OC11");      }
+function OC12(){ BT("OC12");      }
+function OC13(){ BT("OC13");      }
+
 function DXII(){BTall("DXII");  }
 
 function RE(){  BTall("RE");    }
