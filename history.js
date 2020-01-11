bb8eb9aa7f4784c1726f52dcc61572bf4393163b
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2020-01-11 15:35:59 +0800

    Update history.js

--- history.js
+++ history.js
@@ -12,12 +12,7 @@ function BT(a){
     }
     else{document.getElementById(com).style.display="block";} 
 } 
-function OD(){  BTall("OD");    }
-function OD1(){ BT("OD1");      }
-function OD2(){ BT("OD2");      }
-function OD3(){ BT("OD3");      }
-function OD4(){ BT("OD4");      }
-function OD5(){ BT("OD5");      }
+
 
 function MC(){  BTall("MC");    }
 function MC1(){ BT("MC1");      }
@@ -61,15 +56,6 @@ function DD4(){ BT("DD4");  }
 function DD5(){ BT("DD5");  }
 function DD6(){ BT("DD6");  }
 function DD7(){ BT("DD7");  }
-
-function RE(){  BTall("RE");    }
-function RE1(){ BT("RE1");      }
-function RE2(){ BT("RE2");      }
-function RE3(){ BT("RE3");      }
-function RE4(){ BT("RE4");      }
-function RE5(){ BT("RE5");      }
-function RE6(){ BT("RE6");      }
-function RE7(){ BT("RE7");      }
 
 function HK1(){ BT("HK1");      }
 function HK2(){ BT("HK2");      }
