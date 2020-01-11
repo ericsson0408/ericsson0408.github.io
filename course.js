5c7d5df3af28552fbe16781a68eaba4d35662a5e
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2020-01-11 15:35:24 +0800

    Create course.js

---
+++ course.js
@@ -0,0 +1,29 @@
+function BTall(a){
+    var com=a+'all';
+    if( document.getElementById(com).style.display=="block"){
+    document.getElementById(com).style.display="none";
+    }
+    else{document.getElementById(com).style.display="block";} 
+}
+function BT(a){
+    var com=a;
+    if( document.getElementById(com).style.display=="block"){
+    document.getElementById(com).style.display="none";
+    }
+    else{document.getElementById(com).style.display="block";} 
+} 
+function OD(){  BTall("OD");    }
+function OD1(){ BT("OD1");      }
+function OD2(){ BT("OD2");      }
+function OD3(){ BT("OD3");      }
+function OD4(){ BT("OD4");      }
+function OD5(){ BT("OD5");      }
+
+function RE(){  BTall("RE");    }
+function RE1(){ BT("RE1");      }
+function RE2(){ BT("RE2");      }
+function RE3(){ BT("RE3");      }
+function RE4(){ BT("RE4");      }
+function RE5(){ BT("RE5");      }
+function RE6(){ BT("RE6");      }
+function RE7(){ BT("RE7");      }
