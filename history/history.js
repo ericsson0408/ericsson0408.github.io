2960ce6886eaee6e6e72b4e1e2c7176e69b1c547
ericsson0408 <33247649+ericsson0408@users.noreply.github.com>
2020-08-28 17:47:04 +0800

    Update history.js

--- history/history.js
+++ history/history.js
@@ -13,40 +13,9 @@ function BT(a){
     else{document.getElementById(com).style.display="block";} 
 } 
 
-
-function MC(){  BTall("MC");    }
-function MC1(){ BT("MC1");      }
-function MC2(){ BT("MC2");      }
-function MC3(){ BT("MC3");      }
-function MC4(){ BT("MC4");      }
-function MC5(){ BT("MC5");      }
-function MC6(){ BT("MC6");      }
-function MC7(){ BT("MC7");      }
-function MC8(){ BT("MC8");      }
-function MC9(){ BT("MC9");      }
-function MC10(){ BT("MC10");      }
-function MC11(){ BT("MC11");      }
-function MC12(){ BT("MC12");      }
-
-function CP(){  BTall("CP");    }
-function CP1(){ BT("CP1");      }
-function CP2(){ BT("CP2");      }
-function CP3(){ BT("CP3");      }
-
-function OC(){ BTall("OC");     }
-function OC1(){ BT("OC1");      }
-function OC2(){ BT("OC2");      }
-function OC3(){ BT("OC3");      }
-function OC4(){ BT("OC4");      }
-function OC5(){ BT("OC5");      }
-function OC6(){ BT("OC6");      }
-function OC7(){ BT("OC7");      }
-function OC8(){ BT("OC8");      }
-function OC9(){ BT("OC9");      }
-function OC10(){ BT("OC10");      }
-function OC11(){ BT("OC11");      }
-function OC12(){ BT("OC12");      }
-function OC13(){ BT("OC13");      }
+function SV(){  BTall("SV");    }
+function SV1(){ BT("SV1");      }
+function SV2(){ BT("SV2");      }
 
 function DD(){  BTall("DD");  }
 function DD1(){ BT("DD1");  }
